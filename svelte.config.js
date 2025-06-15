c133366d9867fa8ba83b312cf285d3ae1fe9633f
Elvis Hernández <davidvalle.rst@gmail.com>
2025-06-14 22:15:04 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,19 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-			// paths: {
-			// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			// }
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
